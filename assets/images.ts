7dadd22e6a332ee65dd746a61a0e9aea1f5b1f34
vsenzo <vsenzo@hello.com>
2022-01-15 00:39:42 +0000

    added assets gitignore

---
+++ assets/images.ts
@@ -0,0 +1,27 @@
+import me from './me.jpg';
+import SM from './SocialMedia.jpg';
+import SMHover from './SocialMedia.gif';
+import BD from './BabelDrive.png';
+import BDHover from './BabelDrive.png';
+import WA from './WeatherApp.jpg';
+import WAHover from './WeatherApp.gif';
+import RA from './RadarApproach.png';
+import RAHover from './RadarApproach.gif';
+import DR from './DissRoom.png';
+import DB from './DissBed.png';
+import background from '../assets/backgroundPortfolio.jpg';
+
+export const images = {
+    me: me,
+    SocialMedia: SM,
+    SocialMediaHover: SMHover,
+    Babel: BD,
+    BabelHover: BDHover,
+    WeatherApp:WA,
+    WeatherAppHover: WAHover,
+    RadarApproach: RA,
+    RadarApproachHover:RAHover,
+    DissRoom: DR,
+    DissBed: DB,
+    background: background,
+}
